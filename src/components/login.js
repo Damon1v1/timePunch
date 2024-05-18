afdadd73c5994749902fcb6486ea1b361af9a380
Damon1v1 <damonburda96@gmail.com>
2024-05-18 11:42:10 -0700

    login form front-end

---
+++ src/components/login.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import  Form from "react-bootstrap";
+import Calo from '../assets/calo.jpg';
+
+class LogIn extends Component {
+    render () {
+        return (
+            <Card id = 'login'>
+                <Card.Body>
+                    <Card.Img variant="top" src={Calo} />
+                    <Card.Title>Calo time tracking</Card.Title>
+                    <Form.Control type="text" className="empName" placeholder="Name" />
+                    <Button>Login</Button>
+                </Card.Body>
+            </Card>
+        );
+    };
+};
+
+export default LogIn;
