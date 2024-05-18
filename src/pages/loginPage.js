281ed819dbededf44e5f77533f24b0b0ec7cbefc
Damon <damonburda96@gmail.com>
2024-05-18 12:21:00 -0700

    fixed package/ .gitignore

---
+++ src/pages/loginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogIn from '../components/login';
+
+const loginPage = () => {
+    return (
+        <div className='loginPage'>
+            <LogIn />
+        </div>
+    )
+};
+
+export default loginPage;
