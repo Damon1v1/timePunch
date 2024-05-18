cdf5293caaf4ad0d33e4b1ab89ea1ab4f8506156
Damon1v1 <damonburda96@gmail.com>
2024-05-18 10:40:43 -0700

    card established

---
+++ src/components/clockin.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+class clockin extends Component {
+    render () {
+        return  (
+            <Card id = 'clockin'></Card>
+        )
+    }
+}
