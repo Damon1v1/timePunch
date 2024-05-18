014c49379dfb81d35a0259ceab2f6b1e840f1f1e
Damon <damonburda96@gmail.com>
2024-05-18 14:40:11 -0700

    storing user login

--- src/components/login.js
+++ src/components/login.js
@@ -1,22 +1,41 @@
-import React, { Component } from "react";
-import Button from 'react-bootstrap/Button';
+import React, { Component, useState } from "react";
+import { Form, FormControl, Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
-import { Form } from 'react-bootstrap';
 import Calo from '../assets/calo.jpg';
 
-class LogIn extends Component {
-    render () {
-        return (
-            <Card id = 'login'>
-                <Card.Body>
-                    <Card.Img variant="top" src={Calo} />
-                    <Card.Title>Calo time tracking</Card.Title>
-                    <Form.Control type="text" className="empName" placeholder="Name" />
-                    <Button href="/clockedin">Login</Button>
-                </Card.Body>
-            </Card>
-        );
+function LogIn() {
+    const [empName, setEmpName] = useState('');
+    const history = useNavigate();
+
+    const handleInputChange = (event) => {
+        setEmpName(event.target.value);
     };
-};
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Employee name:', empName);
+        history('/clockedin')
+    };
+
+    return (
+        <Card id='login'>
+            <Card.Body>
+                <Card.Img variant="top" src={Calo} />
+                <Card.Title>Calo time tracking</Card.Title>
+                <Form onSubmit={handleSubmit}>
+                    <FormControl
+                        type="text"
+                        className="empName"
+                        placeholder="Name"
+                        value={empName}
+                        onChange={handleInputChange}
+                    />
+                    <Button type="submit" >Login</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+    );
+}
 
 export default LogIn;
