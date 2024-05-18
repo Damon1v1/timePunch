b02dea5caaf2352ce5088faaebc6ff68aadbed51
Damon1v1 <damonburda96@gmail.com>
2024-05-18 10:57:41 -0700

    Clock in card component

--- src/components/clockin.js
+++ src/components/clockin.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Calo from '../assets/calo.jpg';
 
 
-class clockin extends Component {
+class ClockIn extends Component {
     render () {
         return  (
             <Card id = 'clockin'>
-                
+                <Card.Img variant="top" src={Calo} />
+                <Card.Body>
+                    <Card.Title>Calo Clock In</Card.Title>
+                    <Card.Text>Keep Track of Work for Calo Kitchen + Tequila</Card.Text>
+                    <Button>Clock In</Button>
+                </Card.Body>
             </Card>
-        )
-    }
-}
+        );
+    };
+};
+
+export default ClockIn;
