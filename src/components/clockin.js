08b628bd2f51dd4b2a7788fcd02d4dd638b64d2e
Damon <damonburda96@gmail.com>
2024-05-18 14:55:38 -0700

    working clock in/out state

--- src/components/clockin.js
+++ src/components/clockin.js
@@ -5,14 +5,35 @@ import Calo from '../assets/calo.jpg';
 
 
 class ClockIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isClockedIn: localStorage.getItem('isClockedIn') === 'true',
+        };
+    }
+
+    handleClockInOut = () => {
+        const newClockStatus = !this.state.isClockedIn;
+        this.setState({ isClockedIn: newClockStatus }, () => {
+            localStorage.setItem('isClockedIn', newClockStatus.toString());
+        });
+    };
+
     render () {
-        return  (
-            <Card id = 'clockin'>
+        return (
+            <Card id='clockin'>
                 <Card.Img variant="top" src={Calo} />
                 <Card.Body>
                     <Card.Title>Calo Clock In</Card.Title>
                     <Card.Text>Keep Track of Work for Calo Kitchen + Tequila</Card.Text>
-                    <Button>Clock In</Button>
+                    <Button onClick={this.handleClockInOut}>
+                        {this.state.isClockedIn ? 'Clock Out' : 'Clock In'}
+                    </Button>
+                    {this.state.isClockedIn ? (
+                        <p>You are now clocked in.</p>
+                    ) : (
+                        <p>You are not clocked in.</p>
+                    )}
                 </Card.Body>
             </Card>
         );
