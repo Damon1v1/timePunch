259ba30e13e097dd3ed1456771525421a96f7697
Damon <damonburda96@gmail.com>
2024-05-19 17:14:14 -0700

    more secure

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'calo_it',
-    port: 3306,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
 connection.connect((err) => {
